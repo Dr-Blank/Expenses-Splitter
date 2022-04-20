56a5abceac3d20577e22f9192dad9796042a6c4f
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2022-04-20 22:25:53 +0530

    Backend
    - Renamed functions to make them sensible.
    - Introduced roles: allowedToParticipate, Menber, Manager.
    - Introduced potValue to track max funds available.
    - participate method for user to pay baseAmount
      and be added as a group Member.
    - Methods to set user as any roles.
    - Convinience methods to get members count.

--- contracts/FriendsExpenseSplitter.sol
+++ contracts/FriendsExpenseSplitter.sol
@@ -2,60 +2,138 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Ownable {
-    address public owner;
+    address public immutable OWNER;
+    error NotAuthorized();
 
     constructor() {
-        owner = msg.sender;
+        OWNER = msg.sender;
     }
 
     modifier OnlyOwner() {
-        require(msg.sender == owner, "Only owner is allowed");
+        if (msg.sender != OWNER) revert NotAuthorized();
         _;
     }
 }
 
 contract FriendsExpenseSplitter is Ownable {
-    struct Transaction {
-        int256 amount;
-        address from;
-        address to;
-    } // to store transaction
-
-    Transaction[] public LogBook; // record of the transactions
-
-    address[] public ListOfPartipants;
-    mapping(address => bool) public Participants; // if user is a member of the group?
-    mapping(address => int256) public Contributions; // dictionary of how much a participant has spent in the group
-
-    error NotAParticipant(address user);
-
-    constructor() {
-        Participants[msg.sender] = true;
-        super;
+    struct Expense {
+        uint256 amount;
+        address paidBy;
+        string description;
     }
 
-    function addUserToGroup(address _newUser) public OnlyOwner {
-        Participants[_newUser] = true;
+    Expense[] public expensesBook; // record of the expenses
+
+    uint256 public immutable baseAmount;
+    uint256 public potValue; // the current value available to spend in any expenses
+    // FIXME remove public in potValue
+
+    mapping(address => bool) public isAllowedToParticipate; // if user is allowed to join
+    mapping(address => bool) public isAllowedToManage; // if a member can add expenses
+    mapping(address => bool) public isMember; // user is a member of the group, when PAID
+    address[] public listOfMembers;
+
+    mapping(address => uint256) public contributions; // dictionary of how much a participant has spent in the group
+
+    error NotAParticipant(address user);
+    error NotEnoughFunds(uint256 amountRequired, uint256 amountSent);
+    error AlreadyParticipated();
+    error PotEmpty(uint256 expense, uint256 potValue);
+
+    modifier OnlyParticipant() {
+        if (!isAllowedToParticipate[msg.sender]) {
+            revert NotAuthorized();
+        }
+        _;
+    }
+
+    modifier OnlyManager() {
+        if (!isAllowedToManage[msg.sender]) {
+            revert NotAuthorized();
+        }
+        _;
+    }
+
+    constructor() payable {
+        super;
+        // base amount is set here
+        baseAmount = msg.value;
+        potValue = baseAmount;
+
+        // add owner to the participants
+        isAllowedToParticipate[OWNER] = true;
+        isAllowedToManage[OWNER] = true;
+        isMember[OWNER] = true;
+        listOfMembers.push(OWNER);
+    }
+
+    function setUserAsParticipant(address _user, bool _status)
+        external
+        OnlyOwner
+    {
+        isAllowedToParticipate[_user] = _status;
+        isAllowedToManage[_user] = _status ? isAllowedToManage[_user] : false;
+    }
+
+    function setUserAsManager(address _user, bool _status) external OnlyOwner {
+        isAllowedToManage[_user] = _status;
+        isAllowedToParticipate[_user] = _status
+            ? true
+            : isAllowedToParticipate[_user];
+    }
+
+    function participate() external payable {
+        // TODO: don't allow user to participate more than once
+        address user = msg.sender;
+        uint256 funds = msg.value;
+
+        if (!(isAllowedToParticipate[user])) {
+            revert NotAParticipant(user);
+        }
+        if (funds < baseAmount) {
+            revert NotEnoughFunds(baseAmount, funds);
+        }
+
+        if (isMember[user]) revert AlreadyParticipated();
+
+        potValue += funds;
+        isMember[user] = true;
+        listOfMembers.push(user);
+    }
+
+    function addExpense(
+        uint256 _amount,
+        address _paidBy,
+        string memory _description
+    ) public OnlyManager {
+        if (_amount > potValue) revert PotEmpty(_amount, potValue);
+
+        expensesBook.push(Expense(_amount, _paidBy, _description));
+        potValue -= _amount;
+        contributions[_paidBy] += _amount;
+    }
+
+    function getMembersCount() external view OnlyParticipant returns (uint256) {
+        return listOfMembers.length;
+    }
+
+    function getMember(uint256 _index)
+        external
+        view
+        OnlyParticipant
+        returns (address)
+    {
+        return listOfMembers[_index];
     }
 
     function getUserContribution(address _user)
-        public
+        external
         view
-        OnlyOwner
-        returns (int256)
+        OnlyParticipant
+        returns (uint256)
     {
-        if (!Participants[_user]) revert NotAParticipant(_user);
+        if (!isMember[_user]) revert NotAParticipant(_user);
 
-        return Contributions[_user];
-    }
-
-    function addTransaction(
-        int256 _amount,
-        address _from,
-        address _to
-    ) public OnlyOwner {
-        LogBook.push(Transaction(_amount, _from, _to));
-        Contributions[_from] += (Participants[_from] ? _amount : int256(0));
-        Contributions[_to] -= (Participants[_to] ? _amount : int256(0));
+        return contributions[_user];
     }
 }
